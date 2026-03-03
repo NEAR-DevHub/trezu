087a85370863002d68a4546f65dadeb7c9104375
akorchyn <artur.yurii.korchynskyi@gmail.com>
2026-03-03 19:23:44 +0200

    fix: threshold pill should be secondary

--- nt-fe/components/approval-info.tsx
+++ nt-fe/components/approval-info.tsx
@@ -13,18 +13,39 @@ interface ApprovalInfoProps {
     approverAccounts?: string[];
 }
 
-const infoText = "Votes required to approve payment-related requests. Editable in Voting settings.";
+const infoText =
+    "Votes required to approve payment-related requests. Editable in Voting settings.";
 
-export function ApprovalInfo({ variant, requiredVotes: requiredVotesProp, approverAccounts: approverAccountsProp, side }: ApprovalInfoProps) {
+export function ApprovalInfo({
+    variant,
+    requiredVotes: requiredVotesProp,
+    approverAccounts: approverAccountsProp,
+    side,
+}: ApprovalInfoProps) {
     const { treasuryId } = useTreasury();
     const { accountId } = useNear();
-    const { data: policy } = useTreasuryPolicy(requiredVotesProp && approverAccountsProp ? null : treasuryId);
+    const { data: policy } = useTreasuryPolicy(
+        requiredVotesProp && approverAccountsProp ? null : treasuryId,
+    );
 
-    const { requiredVotes, approverAccounts } = requiredVotesProp && approverAccountsProp ? { requiredVotes: requiredVotesProp, approverAccounts: approverAccountsProp } : policy ? getApproversAndThreshold(policy, accountId ?? "", "call", false) : { requiredVotes: 0, approverAccounts: [] };
+    const { requiredVotes, approverAccounts } =
+        requiredVotesProp && approverAccountsProp
+            ? {
+                  requiredVotes: requiredVotesProp,
+                  approverAccounts: approverAccountsProp,
+              }
+            : policy
+              ? getApproversAndThreshold(policy, accountId ?? "", "call", false)
+              : { requiredVotes: 0, approverAccounts: [] };
 
     if (variant === "pupil") {
         return (
-            <Pill title={`Threshold ${requiredVotes} out of ${approverAccounts?.length ?? 0}`} info={infoText} side={side} />
+            <Pill
+                variant="secondary"
+                title={`Threshold ${requiredVotes} out of ${approverAccounts?.length ?? 0}`}
+                info={infoText}
+                side={side}
+            />
         );
     }
 
@@ -33,10 +54,8 @@ export function ApprovalInfo({ variant, requiredVotes: requiredVotesProp, approv
             <Info />
             <AlertDescription className="inline-block">
                 This payment will require approval from{" "}
-                <span className="font-semibold">
-                    {requiredVotes}
-                </span>{" "}
-                treasury members before execution.
+                <span className="font-semibold">{requiredVotes}</span> treasury
+                members before execution.
             </AlertDescription>
         </Alert>
     );
